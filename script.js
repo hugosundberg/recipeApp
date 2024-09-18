a8af0a68465089dc127abb584b6f30257c285838
hugosundberg <hugosundberg98@gmail.com>
2024-09-18 10:35:30 +0200

    get recipes

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-// script.js
+// API SEARCH URL https://api.edamam.com/api/recipes/v2?type=user&q=chicken&app_id=3fac8ac0&app_key=c56415d77325af3a344d29207d9dcdb3
+
+const apiKey = "c56415d77325af3a344d29207d9dcdb3";
+const apiId = "3fac8ac0";
 
 // Function to handle the search button click
 function handleSearch() {
   const searchBar = document.querySelector(".search-bar");
   const query = searchBar.value;
-  return query;
 }
 
 // Add event listener to the search button
 document.addEventListener("DOMContentLoaded", () => {
   const searchButton = document.querySelector(".search-button");
   searchButton.addEventListener("click", handleSearch);
-  displayRecipe();
 });
 
-function displayRecipe() {}
+async function getRecipes() {
+  const apiUrl = `https://api.edamam.com/api/recipes/v2?type=user&q=${chicken}&app_id=${apiId}0&app_key=${apiKey}`;
+
+  const response = await fetch(apiUrl);
+
+  return await response.json();
+}
