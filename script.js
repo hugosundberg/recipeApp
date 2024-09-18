d34d259176358c6087339eef02b5df8251cf1af0
hugosundberg <hugosundberg98@gmail.com>
2024-09-18 11:20:16 +0200

    response

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-// API SEARCH URL https://api.edamam.com/api/recipes/v2?type=user&q=chicken&app_id=3fac8ac0&app_key=c56415d77325af3a344d29207d9dcdb3
-
 const apiKey = "c56415d77325af3a344d29207d9dcdb3";
 const apiId = "3fac8ac0";
 
 // Function to handle the search button click
-function handleSearch() {
+async function handleSearch() {
   const searchBar = document.querySelector(".search-bar");
   const query = searchBar.value;
 
-  getRecipes(query);
+  console.log(query);
+
+  const recipes = await getRecipes(query);
+  const recipeDetails = extractRecipeDetails(recipes);
+
+  displayRecipes(recipeDetails);
 }
 
 // Add event listener to the search button
@@ -18,9 +21,22 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 async function getRecipes(query) {
-  const apiUrl = `https://api.edamam.com/api/recipes/v2?type=user&q=${query}&app_id=${apiId}&app_key=${apiKey}`;
+  const apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${apiId}&app_key=${apiKey}`;
 
   const response = await fetch(apiUrl);
 
   return await response.json();
+}
+
+function extractRecipeDetails(response) {
+  return response.hits.slice(0, 10).map((hit) => ({
+    name: hit.recipe.label,
+    url: hit.recipe.url,
+    image: hit.recipe.image,
+    ingredients: hit.recipe.ingredientLines,
+  }));
+}
+
+function displayRecipes(recipeDetails) {
+  console.log(recipeDetails);
 }
